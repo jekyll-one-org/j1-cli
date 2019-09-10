417f3d715e736943c0f27bb0c7796e39a4988f69
Juergen Adams <juergen@jekyll-one.com>
2019-09-10 18:17:53 +0200

    Initial commit

--- bin/j1.js
+++ bin/j1.js
@@ -1,16 +1,65 @@
 #! /usr/bin/env node
 
-var shell = require("shelljs");
-var yargs = require("yargs");
+/*
+ # -----------------------------------------------------------------------------
+ # ~/bin/j1.js
+ # JS command wrapper for J1 Template
+ #
+ # Product/Info:
+ # https://jekyll.one
+ #
+ # Copyright (C) 2019 Juergen Adams
+ #
+ # J1 Template is licensed under the MIT License.
+ # For details, see https://jekyll.one
+ # -----------------------------------------------------------------------------
+*/
+'use strict';
 
+// ---------------------------------------------------------------------------
+// modules|libraries
+// ---------------------------------------------------------------------------
+var shell = require('shelljs');
+var yargs = require('yargs');
+var getos = require('getos')
+
+// ---------------------------------------------------------------------------
+// globals
+// ---------------------------------------------------------------------------
+var os = '';
+
+
+os = getos(function(e,os) {
+  var os_info = JSON.stringify(os);
+  if(e) return e;
+  return os['os'];
+});
+
+// ---------------------------------------------------------------------------
+// main
+// ---------------------------------------------------------------------------
 var argv = yargs.usage("$0 command")
-  .command("gh", "list repo commit histiory", function (yargs) {
-    shell.exec("git log --pretty=format:'%h %ad | %s%d [%an]' --graph --date=short");
+  .command('gh', 'list repo commit history', function (yargs) {
+    if (os === 'win32') {
+      shell.exec('git log --pretty=format:"%Cred%h%Creset -%C(yellow)%ad%Creset - %Cgreen(%cr)%Creset | %C(bold blue)%s%d [%an]%Creset" --graph --date=short');
+    } else {
+      shell.exec("git log --pretty=format:'%Cred%h%Creset -%C(yellow)%ad%Creset - %Cgreen(%cr)%Creset | %C(bold blue)%s%d [%an]%Creset' --graph --date=short");
+    }
   })
-  .command("gs", "list current status of the repo", function (yargs) {
-    shell.exec("git status");
+  .command('gs', 'list current status of the repo', function (yargs) {
+    shell.exec('git status');
   })
-  .demand(1, "must provide a valid command")
-  .help("h")
-  .alias("h", "help")
-  .argv
+  .command('getos', 'print the name of the operating system', function (yargs) {
+    console.log(os);
+  })
+  .command('pwd', 'print current working directory', function (yargs) {
+    if (os === 'win32') {
+      shell.exec('echo %cd%');
+    } else {
+      shell.exec(pwd);
+    }
+  })
+  .demand(1, 'must provide a valid command')
+  .help('h')
+  .alias('h', 'help')
+  .argv;
